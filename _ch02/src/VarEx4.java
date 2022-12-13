58e3ea7140132c7da5c0b0de25689e197684099f
hoseol OH <dhgh9590@naver.com>
2022-12-13 16:26:02 +0900

    변수 타입

--- _ch02/src/VarEx4.java
+++ _ch02/src/VarEx4.java
@@ -6,7 +6,7 @@ public class VarEx4 {
 		int i = 'A'; //문자를 int에 할당하면 문자의 숫자코드를 int에 저장하는 것과 같음
 		String ch2 = "가나다";
 		String ch3 = "마바사";
-		String ch4 = ch2 + ch3;
+		String ch4 = ch2 + ch3; //문자열 더하기
 		
 		System.out.println(ch4);
 
